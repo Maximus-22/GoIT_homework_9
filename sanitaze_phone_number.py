c12016fcf976b0b03f7030241e49ba576350963b
Maximus-22 <maksim.melnichenko@nservice.com.ua>
2023-08-06 18:03:22 +0300

    Fixed AttributeError
    Added check None in function sanitaze_pnone_number

--- sanitaze_phone_number.py
+++ sanitaze_phone_number.py
@@ -1,12 +1,15 @@
 def sanitaze_phone_number(phone: str) -> str:
-    new_phone = (phone.strip()
-                 .lstrip("+")
-                 # .removeprefix('+')
-                 .replace("(", "")
-                 .replace(")", "")
-                #  .replace(" ", "")
-                 .replace("-", "")
-                 )
+    if phone is None:
+        return None
+    else:
+        new_phone = (phone.strip()
+                    .lstrip("+")
+                    # .removeprefix('+')
+                    .replace("(", "")
+                    .replace(")", "")
+                    #  .replace(" ", "")
+                    .replace("-", "")
+                    )
     return new_phone
 
 
