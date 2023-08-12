2640c7dffd2598879899364d4bb27bd839394e53
Maximus-22 <maksim.melnichenko@nservice.com.ua>
2023-08-12 11:27:46 +0300

    Carring function get_hendler
    + add check plural split string of input_user...

--- pocket_phonebook.py
+++ pocket_phonebook.py
@@ -26,7 +26,10 @@ def handler_com_phone(name: str, *args):
     if name in phonebook:
         print(f"Your contact {name} has a number: {phonebook[name]}")
 
+
 def handler_com_showall(*args):
+    init(autoreset = True)
+    print(Fore.YELLOW + "Current list of all contacts:")
     for name, phone in sorted(phonebook.items()):
         print("{:<18} -> {:<13}".format(name, phone))
 
@@ -38,6 +41,11 @@ def handler_com_help(*args):
                 "Available commands [\"add Name Phone\", \"change Name Phone\",\n" \
                 "\"hello\", \"help\", \"phone Name\", \"showall\"]."
     print(Fore.YELLOW + introduse)
+
+
+def handler_com_exit(*args):
+    init(autoreset = True)
+    print(Fore.YELLOW + "Good bye!")
 
 
 def handler_com_exit(*args):
@@ -63,7 +71,11 @@ COMMANDS = {'add': handler_com_add, 'change': handler_com_change,\
 def input_error(get_handler):
     def wrapper(*args, **kwargs):
         try:
-            return get_handler(*args, **kwargs)
+            error = command_validator(*args, **kwargs)
+            if error:
+                raise error
+            else:
+                return get_handler(*args, **kwargs)
         except KeyError:
             init(autoreset = True)
             print(Fore.YELLOW + "Oops! Key Error.\n" \
@@ -79,6 +91,27 @@ def input_error(get_handler):
     return wrapper
 
 
+@input_error
+def get_handler(command: str, name: str, phone: str):
+    return COMMANDS[command](name, phone)
+
+
+def command_validator(command, name, phone):
+    if command not in COMMANDS:
+        error = KeyError
+    elif command in ("add", "change") and (name is None or phone is None):
+        error = IndexError
+    elif command == "phone" and name is None:
+        error = IndexError
+    elif command in ("add", "change") and not (name.isalpha() and phone.isdigit() and len(phone) >= 10):
+        error = ValueError
+    elif command in ("change", "phone") and name not in phonebook:
+        error = ValueError
+    else:
+        error = None
+    return error
+
+
 def command_parser(command_input: str) -> tuple:
     command_split = command_input.strip().split()
     if len(command_split) == 1:
@@ -88,33 +121,23 @@ def command_parser(command_input: str) -> tuple:
         command = command_split[0].lower()
         name = command_split[1].lower().title() if len(command_split[1]) > 2 else None
         return command, name, None
-    elif len(command_split) == 3:
+    elif len(command_split) >= 3:
         command = command_split[0].lower()
         name = command_split[1].lower().title() if len(command_split[1]) > 2 else None
         phone = command_split[2] if len(command_split[2]) >= 10 else None
         sanitazed_phone = sphn(phone)
         return command, name, sanitazed_phone
+    # else:
+    #     command = command_split[0].lower()
+    #     name = command_split[1].lower().title() if len(command_split[1]) > 2 else None
+    #     phone = command_split[2] if len(command_split[2]) >= 10 else None
+    #     sanitazed_phone = sphn(phone)
+    #     # return command, name, sanitazed_phone
+    # return command, name, sanitazed_phone
     # command = command_split[0].lower() if len(command_split[0]) > 0 else None
     # name = command_split[1].lower().title() if len(command_split[1]) > 2 else None
     # phone = command_split[2] if len(command_split[2]) >= 10 else None
     # return command, name, phone
-
-
-@input_error
-def get_handler(command: str, name: str, phone: str):
-    # command, name, phone = command_parser(command_input)
-    if command not in COMMANDS:
-        raise KeyError
-    elif command in ("add", "change") and (name is None or phone is None):
-        raise IndexError
-    elif command == "phone" and name is None:
-        raise IndexError
-    elif command in ("add", "change") and not (name.isalpha() and phone.isdigit() and len(phone) >= 10):
-        raise ValueError
-    elif command in ("change", "phone") and name not in phonebook:
-        raise ValueError
-    else:
-        return COMMANDS[command](name, phone)
 
 
 def main():
@@ -124,18 +147,23 @@ def main():
                 name, phone = line.strip().split(';')
                 phonebook[name] = phone
     
-
     init(autoreset = True)
-    let_begin = "Please, choose command \"help\" for begin"
-    print(Fore.YELLOW + let_begin)
+    # let_begin = "Please, choose command \"help\" for begin"
+    print(Fore.YELLOW + "Please, choose command \"help\" for begin")
         
     while 1:
         command_input = input("Common your command: ")
+        
         if not command_input:
             continue
-        command, name, phone = command_parser(command_input)
+        
+        (command, name, phone) = command_parser(command_input)
         get_handler(command, name, phone)
+        
         if command in ("exit", "close", "goodbye"):
+            with open("phonebook.txt", "w", encoding = "UTF-8") as file:
+                for name, phone in phonebook.items():
+                    file.write(f"{name};{phone}\n")    
             break
 
 
